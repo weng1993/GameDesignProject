e4100a2b8ff813c1f101059f72cb885ec20881dd
McKenzieW <McKenzieW@users.noreply.github.com>
2016-10-30 17:12:12 -0400

    Update CameraController.cs
    to rotate camera with player

--- Tom/Scripts/CameraController.cs
+++ Tom/Scripts/CameraController.cs
@@ -15,5 +15,6 @@ public class CameraController : MonoBehaviour {
 	void LateUpdate ()
 	{
 		transform.position = player.transform.position + offset;
+		transform.rotation = player.transform.rotation;
 	}
 }
