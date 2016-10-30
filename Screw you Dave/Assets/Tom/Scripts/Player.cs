582e8ddd4f5c048a2eb5d709f6af7a6382155844
Thomas <trnd@comcast.net>
2016-10-30 17:27:54 -0400

    Test Scene

---
+++ Screw you Dave/Assets/Tom/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody rigidbody;
+	Vector3 velocity;
+	float bulletImpulse = 5f;
+	public GameObject projectile_prefab;
+	bool schleem = false;
+	Vector3 fix = new Vector3 (0,1,.5f);
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * 10;
+		//Switch Bodies
+		schleem = Input.GetKeyDown(KeyCode.X);
+		if (schleem) {
+			GameObject projectile = (GameObject)Instantiate (projectile_prefab, transform.position + fix,transform.rotation);
+			projectile.GetComponent<Rigidbody>().AddForce(transform.forward*bulletImpulse, ForceMode.Impulse);
+		}
+	}
+
+	void FixedUpdate() {
+		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Screw you Dave/Assets/Tom/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//make sure player object + terrain have 0 bounciness, unless have 
+	//grounded check and different movement controls
+	public Collider coll;
+	public Rigidbody rb;
+	public float startingTime;
+	private float timeLeft;
+
+	void Start () {
+		timeLeft = startingTime;
+
+		//bounciness 0 (likely included in player already)
+		coll = GetComponent<BoxCollider>();
+		PhysicMaterial material = new PhysicMaterial();
+		material.bounciness = 0;
+		coll.material = material;
+
+		//freeze rotation so object will fly straight
+		rb = GetComponent<Rigidbody> ();
+		rb.freezeRotation = true;
+	}
+
+	void Update () {
+
+		//update flight time
+		timeLeft -= Time.deltaTime;
+
+		//base movement
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 5.0f;
+
+		transform.Rotate (0, x, 0);
+		transform.Translate (0, 0, z);
+
+		//regain flight time when touching ground
+		if (isGrounded ()) {
+			timeLeft = startingTime;
+			rb.useGravity = false;
+		}
+
+		//out of flight time
+		if (timeLeft <= 0) {
+			//re-enable gravity to make object fall
+			rb.useGravity = true;
+		}
+		else {
+			//if flight time left use j to ascend and k to descend
+			if (Input.GetKey ("j")) {
+				transform.Translate (0, 0.1f, 0);
+			} 
+			else if (Input.GetKeyUp ("j")) {
+				transform.Translate (0, 0, 0);
+			}
+
+			if (Input.GetKey ("k")) {
+				transform.Translate (0, -0.1f, 0);
+			} 
+			else if (Input.GetKeyUp ("k")) {
+				transform.Translate (0, 0, 0);
+			}
+		}
+
+	}
+
+	bool isGrounded() {
+		//change 3rd var depending on object size
+		return Physics.Raycast (transform.position, -Vector3.up, 0.6f);
+	}
+}
