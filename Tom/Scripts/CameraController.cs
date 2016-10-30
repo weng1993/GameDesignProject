2425d4e7a0a5403c0488099ad58588e409ca931d
McKenzieW <McKenzieW@users.noreply.github.com>
2016-10-30 17:24:17 -0400

    Update CameraController.cs
    updates y rotations

--- Tom/Scripts/CameraController.cs
+++ Tom/Scripts/CameraController.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	
+	private float x;
+	private float y;
+	private float z;
 
 	private Vector3 offset;
 
 	void Start ()
 	{
 		offset = transform.position - player.transform.position;
+		x = transform.rotation.x;
+		z = transform.rotation.z;
 	}
 
 	void LateUpdate ()
 	{
 		transform.position = player.transform.position + offset;
-		transform.rotation.y = player.transform.rotation.y;
+		transform.rotation = Vector3(x, player.transform.rotation.y, z);
 	}
 }
