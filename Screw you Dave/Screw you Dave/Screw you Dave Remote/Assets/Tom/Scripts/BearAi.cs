de21b41742da14fad671ebacd0dcec86f7bccaab
Eric K. Wyss <ewyss01@vm-hw00.eecs.tufts.edu>
2016-11-13 14:39:56 -0500

    fix?

---
+++ Screw you Dave/Screw you Dave/Screw you Dave Remote/Assets/Tom/Scripts/BearAi.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BearAi : MonoBehaviour {
+
+	public Collider coll;
+	public Rigidbody rb;
+	public float startingTime;
+	private float timeLeft;
+
+	private int meleeDamage;
+	private float meleeRange;
+	private float attackTime;
+	private float cooldown;
+
+	private float specialCD;
+	private float CDTime;
+
+	public Slider healthSlider;
+	public Slider AtkSlider;
+	public Slider CDSlider;
+
+	public bool playerSeen = false;
+	public bool flight = false;
+
+	public Transform player;
+	private float speed = 3f;
+	private float attackSpeed = 6f;
+	private float rotationSpeed = 6f;
+
+	public Transform[] enemyPath = new Transform[12];
+	public int pathNum = 0;
+	public bool alive = true;
+
+	public GameObject home;
+
+	void Start () {
+		timeLeft = startingTime;
+		//bounciness 0 (likely included in player already)
+		coll = GetComponent<BoxCollider>();
+		PhysicMaterial material = new PhysicMaterial();
+		material.bounciness = 0;
+		coll.material = material;
+
+		//freeze rotation so object will fly straight
+		rb = GetComponent<Rigidbody> ();
+		rb.freezeRotation = true;
+
+		meleeRange = 4;
+		meleeDamage = 30;
+		attackTime = 0;
+		cooldown = .5f;
+
+		specialCD = 1;
+		CDTime = 0;
+		//UI
+		healthSlider = GameObject.Find("healthSlider").GetComponent<Slider>();
+		AtkSlider = GameObject.Find ("AtkSlider").GetComponent<Slider>();
+		CDSlider = GameObject.Find ("CDSlider").GetComponent<Slider> ();
+
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () {
+		if (alive) {
+			if (playerSeen) {
+				attackPlayer ();
+			} else {
+				idle ();
+			}
+		}
+	}
+
+	bool isGrounded() {
+		//change 3rd var depending on object size
+		return Physics.Raycast (transform.position, -Vector3.up, 0.6f);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.tag == "enemyPath"){
+			if (pathNum < 11) {
+				pathNum++;
+			} else {
+				pathNum = 0;
+			}
+
+		}
+	}
+
+	void attackPlayer(){
+		Quaternion rotation = Quaternion.LookRotation (player.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+		if(Vector3.Distance(transform.position,player.position) > meleeRange) {
+			transform.Translate (0,0,Time.deltaTime*speed);
+		}
+		else{
+			if (attackTime > 0)
+				attackTime -= Time.deltaTime;
+			if (attackTime <= 0) {
+				attack ();
+				attackTime = cooldown;
+			}
+		}
+	}
+
+	void idle(){
+		Quaternion rotation = Quaternion.LookRotation (enemyPath[pathNum].position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+		Vector2 pathDirection = enemyPath [pathNum].position - transform.position;
+		float speedElement = Vector2.Dot (pathDirection.normalized,transform.forward);
+		transform.Translate (0,0,Time.deltaTime*attackSpeed);
+	}
+
+	void attack() {
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		if (Physics.Raycast(transform.position, fwd, out hit, meleeRange) && (hit.transform.tag == "Player" || hit.transform.tag == "Bird" || hit.transform.tag == "Bear" || hit.transform.tag == "Turtle")) {
+			hit.transform.gameObject.GetComponent<Health2>().adjustHealth (-meleeDamage);
+			if (hit.transform.gameObject.GetComponent<Health2>().health <= 0){
+				//Game over
+			}
+		}
+	}
+}
