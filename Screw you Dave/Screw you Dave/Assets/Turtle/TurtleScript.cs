090000f461798c363e50b7014bdc7fb31ab8d54b
McKenzieW <McKenzieW@users.noreply.github.com>
2016-12-05 17:27:33 -0500

    updated turtle scene

--- Screw you Dave/Screw you Dave/Assets/Turtle/TurtleScript.cs
+++ Screw you Dave/Screw you Dave/Assets/Turtle/TurtleScript.cs
@@ -11,7 +11,7 @@ public class TurtleScript : MonoBehaviour {
 	public GameObject projectile_prefab;
 	public GameObject claw_prefab;
 	bool schleem = false;
-	Vector3 fix = new Vector3 (0,0.5f,.5f);
+	Vector3 fix = new Vector3 (0,0.5f,0);
 
 	private int meleeDamage;
 	private float meleeRange;
@@ -23,9 +23,9 @@ public class TurtleScript : MonoBehaviour {
 	public Slider CDSlider;	*/
 
 	public bool underwater = false;
-	private float underwaterYOffset;
-	private float toAboveY;
-	private float abovewaterY;
+	public float underwaterYOffset;
+	public float toAboveY;
+	public float abovewaterY;
 	public float startingTime;
 	private float timeLeft;
 
@@ -52,7 +52,7 @@ public class TurtleScript : MonoBehaviour {
 		AtkSlider = GameObject.Find ("AtkSlider").GetComponent<Slider>();
 		CDSlider = GameObject.Find ("CDSlider").GetComponent<Slider> ();*/
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * 10;
@@ -60,8 +60,8 @@ public class TurtleScript : MonoBehaviour {
 		//Switch Bodies
 		schleem = Input.GetKeyDown("space");
 		if (schleem) {
-			GameObject projectile = (GameObject)Instantiate (projectile_prefab, transform.position+fix,Camera.main.transform.rotation);
-			projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward*bulletImpulse, ForceMode.Impulse);
+			GameObject projectile = (GameObject)Instantiate (projectile_prefab, transform.position+fix,transform.rotation);
+			projectile.GetComponent<Rigidbody>().AddForce(transform.forward*bulletImpulse, ForceMode.Impulse);
 		}
 
 		//Makes sure turtle stays on ground at switch
@@ -73,10 +73,11 @@ public class TurtleScript : MonoBehaviour {
 		/* timeLeft -= Time.deltaTime; */
 
 		//base movement
-		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 10.0f;
+		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 10.0f;
 
-		transform.Translate (x, 0, z);
+		transform.Rotate (0, x, 0);
+		transform.Translate (0, 0, z);
 
 		//out of swim time
 		/* if (timeLeft <= 0) {
@@ -84,19 +85,19 @@ public class TurtleScript : MonoBehaviour {
 			rb.useGravity = true;
 			// and pop turtle out of water (still need to write this code)
 		}else { */
-			// if swim time left use j to ascend and k to descend
-			// remove gravity when in water
+		// if swim time left use j to ascend and k to descend
+		// remove gravity when in water
 		if (Input.GetMouseButton (1)) {
-				rb.useGravity = false;
-				underwater = true;
-				Vector3 v = rb.velocity;
-				v.y = 0;
-				rb.velocity = v;
-				transform.Translate (0, 0.1f, 0);
-			} 
+			rb.useGravity = false;
+			underwater = true;
+			Vector3 v = rb.velocity;
+			v.y = 0;
+			rb.velocity = v;
+			transform.Translate (0, 0.1f, 0);
+		} 
 		else if (Input.GetMouseButtonUp (1)) {
-				rb.useGravity = true;
-			}
+			rb.useGravity = true;
+		}
 		//}
 
 		if (attackTime > 0)
@@ -112,51 +113,27 @@ public class TurtleScript : MonoBehaviour {
 		/*healthSlider.value = (this.gameObject.GetComponent<Health2>().health / (float)this.gameObject.GetComponent<Health2>().maxHealth);
 		AtkSlider.value = 1 - (attackTime / cooldown);
 		CDSlider.value = (timeLeft / startingTime);*/
-
-		fix.z = 0.5f * Mathf.Cos (Camera.main.transform.eulerAngles.y * Mathf.Deg2Rad);
-		fix.x = .5f * Mathf.Sin (Camera.main.transform.eulerAngles.y * Mathf.Deg2Rad);
 	}
 
 	void attack() {
 		RaycastHit hit;
-		bool collided = false;
-
-		Vector3 dir = Camera.main.transform.TransformDirection (Vector3.forward);
-		Vector3 dir2 =Camera.main.transform.TransformDirection (Vector3.forward) + new Vector3 (0, .1f, 0);
-		Vector3 dir3 =Camera.main.transform.TransformDirection (Vector3.forward) + new Vector3 (-.1f, 0, 0);
-		Vector3 dir4 =Camera.main.transform.TransformDirection (Vector3.forward) + new Vector3 (0, -.1f, 0);
-		Vector3 dir5 =Camera.main.transform.TransformDirection (Vector3.forward) + new Vector3 (.1f, 0, 0);
-		Vector3[] dirs = { dir, dir2, dir3, dir4, dir5 };
-
-
-		for (int i = 0; i < dirs.Length; i++) {
-			//Debug.DrawRay (transform.position+fix, dirs[i] * meleeRange, Color.cyan, 3);
-			if (Physics.Raycast(transform.position+fix, dirs[i], out hit, meleeRange) && (hit.transform.tag == "AIPlayer" || hit.transform.tag == "Bird" || hit.transform.tag == "Bear" || hit.transform.tag == "Turtle") && collided == false) {
-				collided = true;
-				hit.transform.gameObject.GetComponent<Health2>().adjustHealth (-meleeDamage);
-				if (hit.transform.gameObject.GetComponent<Health2>().health <= 0){
-					if (hit.transform.tag.ToLower() == "bird") {
-						if(hit.transform.gameObject.GetComponent<BirdAi> () != null){
-							hit.transform.gameObject.GetComponent<BirdAi> ().alive = false;
-							if(hit.transform.gameObject.GetComponent<BirdAi> ().home.GetComponent<EnemyHome> () != null){
-								Destroy (hit.transform.gameObject.GetComponent<BirdAi> ().home.GetComponent<EnemyHome> ());
-							}
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		if (Physics.Raycast(transform.position, fwd, out hit, meleeRange) && (hit.transform.tag == "AIPlayer" || hit.transform.tag == "Bird" || hit.transform.tag == "Bear" || hit.transform.tag == "Turtle")) {
+			hit.transform.gameObject.GetComponent<Health2>().adjustHealth (-meleeDamage);
+			if (hit.transform.gameObject.GetComponent<Health2>().health <= 0){
+				if (hit.transform.tag.ToLower() == "bird") {
+					if(hit.transform.gameObject.GetComponent<BirdAi> () != null){
+						hit.transform.gameObject.GetComponent<BirdAi> ().alive = false;
+						if(hit.transform.gameObject.GetComponent<BirdAi> ().home.GetComponent<EnemyHome> () != null){
+							Destroy (hit.transform.gameObject.GetComponent<BirdAi> ().home.GetComponent<EnemyHome> ());
 						}
 					}
-					if (hit.transform.tag.ToLower() == "bear") {
-						if(hit.transform.gameObject.GetComponent<BearAi> () != null){
-							hit.transform.gameObject.GetComponent<BearAi> ().alive = false;
-							if (hit.transform.gameObject.GetComponent<BearAi> ().home.GetComponent<EnemyHome> () != null) {
-								Destroy (hit.transform.gameObject.GetComponent<BearAi> ().home.GetComponent<EnemyHome> ());
-							}
-						}
-					}
-					if (hit.transform.tag.ToLower() == "turtle") {
-						if(hit.transform.gameObject.GetComponent<TurtleAi> () != null){
-							hit.transform.gameObject.GetComponent<TurtleAi> ().alive = false;
-							if (hit.transform.gameObject.GetComponent<TurtleAi> ().home.GetComponent<EnemyHome> () != null) {
-								Destroy (hit.transform.gameObject.GetComponent<TurtleAi> ().home.GetComponent<EnemyHome> ());
-							}
+				}
+				if (hit.transform.tag.ToLower() == "bear") {
+					if(hit.transform.gameObject.GetComponent<BearAi> () != null){
+						hit.transform.gameObject.GetComponent<BearAi> ().alive = false;
+						if (hit.transform.gameObject.GetComponent<BearAi> ().home.GetComponent<EnemyHome> () != null) {
+							Destroy (hit.transform.gameObject.GetComponent<BearAi> ().home.GetComponent<EnemyHome> ());
 						}
 					}
 				}
@@ -167,12 +144,14 @@ public class TurtleScript : MonoBehaviour {
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.CompareTag ("Water")) {
-			if (underwater = false) {
-				underwater = true;
-				rb.useGravity = false;
+			Debug.Log ("collides with water");
+			if (!underwater) {
+				Debug.Log("goes underwater");
 				transform.Translate (0, underwaterYOffset, 0);
+				underwater = true;
 			} else {
 				if (transform.position.y < toAboveY) {
+					Debug.Log ("goes above water");
 					if (transform.position.x < other.gameObject.transform.position.x) {
 						transform.Translate (-5f, (abovewaterY - transform.position.y), 0);
 					} else {
