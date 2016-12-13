357d76e2ddc0c76fdc19b1060e1e8c88dd10998e
Thomas <trnd@comcast.net>
2016-12-13 13:26:01 -0500

    Adding Level

---
+++ Screw you Dave/Screw you Dave/Assets/Tom/Scripts/Win.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			if (col.gameObject.GetComponent<DaveScript> () != null) {
+				//Win Game
+			}
+		}
+	}
+
+}
